42c0e91af5e37798725d125bf131bfecacef47be
Hetal <hetalsonani22@gmail.com>
2020-11-18 16:04:00 -0800

    added changes

---
+++ src/Java_Interview_Questions/Addtwonumbers.java
@@ -0,0 +1,13 @@
+package Java_Interview_Questions;
+
+public class Addtwonumbers {
+
+	public static void main(String[] args) {
+		int a =10;
+		int b =20;
+		int sum = a+b;
+		System.out.println(sum);
+
+	}
+
+}
